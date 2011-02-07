1d5d420e8dec01a66aecf42b43ce098bc99870b8
Andrew Sutherland <asutherland@asutherland.org>
2011-02-07 05:42:07 -0800

    UI displays the list of known canned data dirs as logins.
    Various UI framework and speculative datastore (in the form of
    comments) buildout.

---
+++ clients/common/deuxnius/lib/deuxnius/ui-main.js
@@ -0,0 +1,273 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at:
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla Messaging Code.
+ *
+ * The Initial Developer of the Original Code is
+ *   The Mozilla Foundation
+ * Portions created by the Initial Developer are Copyright (C) 2011
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Andrew Sutherland <asutherland@asutherland.org>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+define(
+  [
+    "wmsy/wmsy",
+    "./unifile",
+    "./pwomise",
+    "exports"
+  ],
+  function(
+    $wmsy,
+    $unifile,
+    $pwomise,
+    exports
+  ) {
+
+var wy = exports.wy = new $wmsy.WmsyDomain({id: "ui-main",
+                                            domain: "deuxnius",
+                                            clickToFocus: true});
+
+var when = $pwomise.when;
+
+wy.defineStyleBase("states", [
+  ".statusText {",
+  "  display: block;",
+  "  font-family: sans-serif;",
+  "  font-size: 200%;",
+  "  text-align: center;",
+  "}",
+]);
+
+wy.defineStyleBase("pages", [
+  ".pageHeader {",
+  "  display: block;",
+  "  font-family: sans-serif;",
+  "  font-size: 150%;",
+  "  text-align: center;",
+  "  background-color: lightblue;",
+  "  border: 1px solid blue;",
+  "  border-radius: 4px;",
+  "  padding: 4px;",
+  "}",
+]);
+
+wy.defineWidget({
+  name: "app-root",
+  constraint: {
+    type: "app-root",
+  },
+  focus: wy.focus.domain.vertical("contents"),
+  structure: {
+    contents: wy.widget({type: "app-state"}, wy.SELF),
+  },
+  impl: {
+    postInit: function() {
+      // Tell the binding about us so it can tell us to update when it changes
+      //  its state.
+      this.obj.binding = this;
+    }
+  },
+});
+
+wy.defineWidget({
+  name: "app-state-probing",
+  constraint: {
+    type: "app-state",
+    obj: { state: "probing" },
+  },
+  structure: {
+    statusText: "Probing server...",
+  },
+  style: {
+    statusText: ".statusText;",
+  },
+});
+
+wy.defineWidget({
+  name: "app-state-error",
+  constraint: {
+    type: "app-state",
+    obj: { state: "error" },
+  },
+  structure: {
+    statusText: "Something bad happened...",
+    overview: wy.bind("message"),
+  },
+  style: {
+    statusText: [
+      ".statusText;",
+      "color: red;",
+    ],
+  },
+});
+
+wy.defineWidget({
+  name: "app-state-login",
+  constraint: {
+    type: "app-state",
+    obj: { state: "login" },
+  },
+  focus: wy.focus.container.vertical("peeps"),
+  structure: {
+    loginLabel: "Who are you?",
+    peeps: wy.vertList({type: "login-peep"}, "loginPeeps"),
+  },
+  events: {
+    peeps: {
+      command: function(peepBinding) {
+        this.obj.loginAs(peepBinding.obj);
+      },
+    },
+  },
+});
+
+wy.defineWidget({
+  name: "login-peep",
+  constraint: {
+    type: "login-peep",
+  },
+  focus: wy.focus.item,
+  structure: {
+    email: wy.bind("email"),
+  },
+  style: {
+    email: [
+      "text-align: center;",
+      "color: seagreen;",
+    ],
+  },
+});
+
+
+wy.defineWidget({
+  name: "app-state-chewing",
+  constraint: {
+    type: "app-state",
+    obj: { state: "chewing" },
+  },
+  structure: {
+    statusText: "Processing messages...",
+    progressText: [wy.bind("soFar"), " of ", wy.bind("total"), " messages."],
+  },
+});
+
+/**
+ * UI fully configured steady-state that uses a page idiom; intended to support
+ *  a mobile idiom but perhaps mappable to a tabbed-style interface too?
+ *
+ * Potential transition idioms:
+ * - Hierarchical descent (ex: contact => conversation)
+ * - Modal dialog style (ex: define a new tag)
+ * - Popup style (ex: select a tag from a big list)
+ */
+wy.defineWidget({
+  name: "app-state-pager",
+  constraint: {
+    type: "app-state",
+    obj: { state: "pager" },
+  },
+  structure: {
+    page: wy.widget({type: "page"}, "curPage"),
+  },
+  receive: {
+    gotoPage: function(pageDef) {
+      this.obj.curPage = pageDef;
+      this.update();
+    },
+  },
+});
+
+/**
+ * Drives the initial connection / setup process.
+ */
+function App(dataDirUrl) {
+  this.dataDirUrl = dataDirUrl;
+
+  this.state = "probing";
+  this.binding = null;
+
+  this.loginPeeps = null;
+  this.fetchLoginCandidates();
+}
+App.prototype = {
+  _explode: function(why) {
+    this.state = "error";
+    this.message = why;
+
+    this.update();
+  },
+
+  update: function() {
+    if (this.binding)
+      this.binding.update();
+  },
+
+  /**
+   * Get the list of pretend users...
+   */
+  fetchLoginCandidates: function() {
+    var self = this;
+    when($unifile.list(this.dataDirUrl), function yay(files) {
+      if (!files.length) {
+        self._explode("Server has no login candidates for us...");
+        return;
+      }
+
+      var peeps = self.loginPeeps = [];
+      for (var i = 0; i < files.length; i++) {
+        var file = files[i];
+        peeps.push({email: file.name});
+      }
+
+      self.state = "login";
+      self.update();
+    }, function boo() {
+      // (actually, unifile just resolves an empty list...)
+      self._explode("Couldn't get data dir contents");
+    });
+  },
+
+  /**
+   * Point the importer at the login peep's directory.
+   */
+  loginAs: function(loginPeep) {
+  },
+
+
+};
+
+exports.main = function main() {
+  var emitter = wy.wrapElement(document.getElementById("body"));
+
+  var objRoot = new App("../../data");
+
+  emitter.emit({type: "app-root", obj: objRoot});
+};
+
+}); // end define
---
+++ clients/common/deuxnius/lib/deuxnius/unifile.js
@@ -0,0 +1,137 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at:
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla Messaging Code.
+ *
+ * The Initial Developer of the Original Code is
+ *   The Mozilla Foundation
+ * Portions created by the Initial Developer are Copyright (C) 2010
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Andrew Sutherland <asutherland@asutherland.org>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+/**
+ * Unified file interface; lets us enumerate the contents of web and local
+ *  directories and get their contents.
+ **/
+
+define(
+  [
+    "exports",
+    "./pwomise",
+  ],
+  function (
+    exports,
+    pwomise
+  ) {
+
+// apache always uses "
+var RE_LINK = /href="([^\"]+)"/g;
+
+function WebFile(aBasePath, aName, aIsDir) {
+  this._base = aBasePath;
+  this.name = aName;
+  this.isDir = aIsDir;
+}
+WebFile.prototype = {
+  toString: function() {
+    return this.name + (this.isDir ? "/" : "");
+  }
+};
+
+/**
+ * List the contents of a web directory by hitting the page, hoping it's an
+ *  index, and treating all relative links observed as children of that
+ *  directory.  If there is a trailing slash, we presume it to be a directory,
+ *  otherwise a file.  Links that start with "?" are presumed to be sorting
+ *  magic or what have you.
+ *
+ * This is intended to handle apache's mod_autoindex output and nothing else:
+ *  http://httpd.apache.org/docs/2.2/mod/mod_autoindex.html
+ *
+ * It would be neat to support other things in the future, but it's not a
+ *  concern right now.
+ */
+function webList(aPath, XHRImpl) {
+  var deferred = pwomise.defer("webList", aPath);
+  var req = new (XHRImpl || XMLHttpRequest)();
+  req.open("GET", aPath, true);
+  req.addEventListener("load", function() {
+    if (req.status != 200) {
+      deferred.resolve([]);
+      return;
+    }
+
+    var match;
+    var things = [];
+    while ((match = RE_LINK.exec(req.responseText))) {
+      var link = match[1];
+      if (!link.length || link[0] === "?" || link[0] === "/" ||
+          link[0] === "." || link.indexOf("//") != -1)
+        continue;
+      // After that comprehensive and absolutely infallible set of heuristics,
+      //  we must be looking at a relative link.
+      var isDir = false;
+      if (link[link.length - 1] === "/") {
+        isDir = true;
+        link = link.substring(0, link.length - 1);
+      }
+      things.push(new WebFile(aPath, link, isDir));
+    }
+    deferred.resolve(things);
+  }, false);
+  req.send(null);
+  return deferred.promise;
+}
+
+var RE_HTTP = /^http[s]?:\/\//;
+
+function LocalFile(aFullPath) {
+}
+LocalFile.prototype = {
+  get isDir() {
+
+  }
+};
+
+exports.normFile = function(aPathy) {
+
+};
+
+exports.list = function(aPath, XHRImpl) {
+  //if (RE_HTTP.test(aPath)) {
+    return webList(aPath, XHRImpl);
+  //}
+  //return file.list(aPath);
+};
+
+exports.listMatchingDescendants = function(aPath) {
+
+};
+
+}); // end define
