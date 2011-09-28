08c63f9ab4b91b592d036d29c7fe192cd8519ec3
Andrew Sutherland <asutherland@asutherland.org>
2011-09-27 17:56:18 -0700

    clone-modify same-proc-group-messaging test to have 1-server variant
    There aren't a lot of differences in paths this way, but it's good to
    have this tested.

--- servers/test/same-proc-group-messaging.js
+++ servers/test/same-proc-group-messaging.js
@@ -101,7 +101,7 @@ TD.commonCase('group messaging upgrade from one-on-one', function(T) {
 
   client_b.do_replyToConversationWith(conv, msg_b1);
 
-  T.group("A invites C");
+  T.group("B invites C");
 
   client_b.do_inviteToConversation(client_c, conv);
 
@@ -109,47 +109,72 @@ TD.commonCase('group messaging upgrade from one-on-one', function(T) {
 
   client_b.do_replyToConversationWith(conv, msg_b2);
 
-// the commented out stuff was part of the original plan, the main deviation
-//  from the plan right now is that we do not do any form of permutation.
+  T.group("C sends a message, all hear");
 
-  /*
-  T.permutation([
-    T.action('The conversation hoster,', client_a, 'invites superfriend',
-             client_c, 'to the conversation', function() {
-      client_a.inviteToConv([client_c], conv);
-    }),*/
-/*    T.action('A participant in the coversation,', client_b,
-             'invites superfriend', client_c, 'to the conversation',
-             function() {
-      client_b.inviteToConv([client_c], conv);
-    })
-*/
-  /*,
-  ])*/;
+  client_c.do_replyToConversationWith(conv, msg_b2);
 
-/*
-  T.action(client_c, 'joins', conv, 'and receives the earlier messages:',
-           msg_a1, msg_b1, function() {
-    client_c.joinConv(join_msg, conv);
-    client_c.expect_receiveMessages([msg_a1, msg_b1]);
-  });
-
-  T.action(client_a, client_b, 'hear about the joining', function() {
-    join_msg.expect_receivedBy([client_a, client_b]);
-  });
-
-  T.action(client_b, 'sends message', msg_b2, 'as part of', conv,
-           'and it is received by', client_a, client_c, function() {
-    client_b.replyToMessageWith(msg_b1, msg_b2);
-    msg_b2.expect_receivedBy([client_a, client_c]);
-  });
-  T.action(client_c, 'sends message', msg_c1, 'as part of', conv,
-           'and it is received by', client_a, client_b, function() {
-    client_c.replyToMessageWith(msg_b2, msg_c1);
-    msg_c1.expect_receivedBy([client_a, client_b]);
-  });
-*/
   T.group('cleanup');
 });
+
+// This is basically the above test but changed so everyone is using the same
+//  server.
+TD.commonCase('same server', function(T) {
+  T.group('setup');
+
+  var client_a = T.actor('testClient', 'A'),
+      client_b = T.actor('testClient', 'B'),
+      client_c = T.actor('testClient', 'C');
+
+  var serverOpts = {roles: ['auth', 'signup', 'drop', 'sender', 'store',
+                            'fanout']};
+  var server_x = T.actor('testServer', 'X', serverOpts);
+  // (all of the above entities have their own initialization steps)
+
+  // the messages in play...
+  var conv = T.thing('conversation', 'conv'),
+      msg_a1 = T.thing('message', 'a1'),
+      msg_b1 = T.thing('message', 'b1'),
+      msg_b2 = T.thing('message', 'b2'),
+      msg_c1 = T.thing('message', 'c1');
+
+
+  client_a.setup_useServer(server_x);
+  client_b.setup_useServer(server_x);
+  client_c.setup_useServer(server_x);
+
+  client_a.setup_connect();
+  client_b.setup_connect();
+  client_c.setup_connect();
+
+  T.group("setup: make friend clique: A, B, C");
+
+  // make everybody already be friends with everybody else
+  // XXX this would ideally be one of our permutations or just an additional
+  //  explicit step (to invite someone who is not a friend of everyone else)
+  client_a.setup_friendClique([client_b, client_c]);
+
+  T.group("start 1:1 conversation between A, B");
+
+  client_a.do_startConversation(conv, msg_a1, [client_b]);
+
+  T.group("B responds to the conversation");
+
+  client_b.do_replyToConversationWith(conv, msg_b1);
+
+  T.group("B invites C");
+
+  client_b.do_inviteToConversation(client_c, conv);
+
+  T.group("B sends a message, all hear");
+
+  client_b.do_replyToConversationWith(conv, msg_b2);
+
+  T.group("C sends a message, all hear");
+
+  client_c.do_replyToConversationWith(conv, msg_b2);
+
+  T.group('cleanup');
+});
+
 
 }); // end define
