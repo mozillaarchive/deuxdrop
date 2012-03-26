df9a4a02383ec4ce6a6d5f564a7f49a99844e93f
Andrew Sutherland <asutherland@asutherland.org>
2012-03-25 23:33:28 -0700

    The sync script needs to look at non-local tags.

--- sync-repo.sh
+++ sync-repo.sh
@@ -2,4 +2,4 @@
 
 # synchronize stuff from the development branch
 
-git checkout develop -- clients/mobileui/ clients/deps/less.js clients/deps/jquery.js styling/server-icons/
+git checkout origin/develop -- clients/mobileui/ clients/deps/less.js clients/deps/jquery.js styling/server-icons/
